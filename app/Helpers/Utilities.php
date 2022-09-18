cc1290a523e4257b023f5e900da55b0d5dd0d61b
Denish Sharma <denishsharma@ternaengg.ac.in>
2022-09-19 01:39:59 +0530

    update(helper-functions): adding helper utilities
    Author: Denish Sharma

---
+++ app/Helpers/Utilities.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Helpers;
+
+class Utilities {
+    public static function formatBytes($size, $precision = 2): string {
+        if ($size > 0) {
+            $size = (int)$size;
+            $base = log($size) / log(1024);
+            $suffixes = array(' bytes', ' KB', ' MB', ' GB', ' TB');
+
+            return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
+        } else {
+            return $size;
+        }
+    }
+}
